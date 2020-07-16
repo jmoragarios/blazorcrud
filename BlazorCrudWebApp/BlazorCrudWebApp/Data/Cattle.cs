00078e32075dc440f44d6e7ed4547ed4686b7c6c
jmoragarios <jmoragarios@gmail.com>
2020-07-16 13:29:53 -0600

    Added front-end project
    Every core of the front-end project and the views of list and create
    Cattles

---
+++ BlazorCrudWebApp/BlazorCrudWebApp/Data/Cattle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrudWebApp.Data
+{
+    public class Cattle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+    }
+}
---
+++ BlazorCrudWebApp/BlazorCrudWebApp/Data/Response.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrudWebApp.Data
+{
+    public class Response
+    {
+        public int Success { get; set; }
+        public string Message { get; set; }
+        public List<Cattle> Data { get; set; }
+
+        public Response()
+        {
+            this.Success = 0;
+        }
+    }
+}
