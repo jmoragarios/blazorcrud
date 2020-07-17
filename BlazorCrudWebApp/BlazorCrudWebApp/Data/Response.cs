256c2d21ca37b379a8757e902b399707329a9a5e
jmoragarios <jmoragarios@gmail.com>
2020-07-16 21:02:52 -0600

    Added Update and Delete
    Also we make the same changes than the api for the generic types of the
    Response

--- BlazorCrudWebApp/BlazorCrudWebApp/Data/Response.cs
+++ BlazorCrudWebApp/BlazorCrudWebApp/Data/Response.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace BlazorCrudWebApp.Data
 {
-    public class Response
+    public class Response<T>
     {
         public int Success { get; set; }
         public string Message { get; set; }
-        public List<Cattle> Data { get; set; }
+        public T Data { get; set; }
 
         public Response()
         {
